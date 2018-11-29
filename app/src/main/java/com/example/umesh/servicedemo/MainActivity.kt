5a24529e1850ac2171ce49e04338afe2d8ad23ec
umeshchandrashakya <ucs9621@gmail.com>
2018-11-29 23:12:02 +0530

    completed

--- app/src/main/java/com/example/umesh/servicedemo/MainActivity.kt
+++ app/src/main/java/com/example/umesh/servicedemo/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.umesh.servicedemo
 
+import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_service.setOnClickListener {
+            intent = Intent(this,MyService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            }else{
+                startService(intent)
+            }
+        }
+
+        stop_service.setOnClickListener {
+            stopService(intent)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(intent)
+
     }
 }
--- app/src/main/java/com/example/umesh/servicedemo/MyService.kt
+++ app/src/main/java/com/example/umesh/servicedemo/MyService.kt
@@ -1,10 +1,23 @@
 package com.example.umesh.servicedemo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import android.util.Log
 
 class MyService:Service(){
+
+    lateinit var mediaPlayer:MediaPlayer
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -12,14 +25,47 @@ class MyService:Service(){
 
     override fun onCreate() {
         super.onCreate()
+        val channelId = if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            createNotificationChannel()
+        }else{
+            ""
+        }
+        val notificationBuilder = NotificationCompat.Builder(this,channelId)
+        val notification = notificationBuilder
+            .setOngoing(true)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setPriority(NotificationManager.IMPORTANCE_MIN)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .build()
+        startForeground(101,notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId="my_service"
+        val channelName ="my back ground service"
+        val chan = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH)
+        chan.lightColor = Color.BLUE
+        chan.importance = NotificationManager.IMPORTANCE_NONE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI)
+        mediaPlayer.start()
+        Log.d("Service","Service started")
+        return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        mediaPlayer.stop()
+        stopSelf()
     }
 
 }

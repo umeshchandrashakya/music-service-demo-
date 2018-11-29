120e74ecd8741f0a30cf0a2f0d8896add0224964
umeshchandrashakya <ucs9621@gmail.com>
2018-11-29 22:37:17 +0530

    intial commit

---
+++ app/src/main/java/com/example/umesh/servicedemo/MyService.kt
@@ -0,0 +1,25 @@
+package com.example.umesh.servicedemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService:Service(){
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
